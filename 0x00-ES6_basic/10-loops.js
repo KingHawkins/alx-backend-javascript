2b37bcc264555e6f4ff41c4a2d344d4af05dd387
KingHawkins <Kinghawkins61@gmail.com>
2023-06-26 21:09:20 +0300

    Pushing all tasks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue (array, appendString) {
+  const store = [];
+  for (const idx of array) {
+    store.push(appendString + idx);
+  }
+
+  return store;
+}
---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject (report) {
+  const array = [];
+  [...Object.values(report.allEmployees)].forEach(value => {
+    for (const item of value) {
+      array.push(item);
+    }
+  }
+  );
+  return array;
+}
